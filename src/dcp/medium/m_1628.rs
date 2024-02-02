fcebc8a7aacdc5b945a2f4a5964f68b76e70d1ef
OnlyFerris <153568011+OnlyFerris@users.noreply.github.com>
2024-02-02 01:58:10 +0000

    Add comments with start and end state for 4x4 matrix rotation.

--- src/dcp/medium/m_1628.rs
+++ src/dcp/medium/m_1628.rs
@@ -70,6 +70,18 @@ fn rotates_3x3_as_expected() {
 
 pub fn rotate_4x4(_m: &mut [u64; 16]) {
     // TODO: Implement
+
+    // [[1, 2, 3, 4],
+    //  [5, 6, 7, 8],
+    //  [9, A, B, C],
+    //  [D, E, F, G]]
+
+    // ...
+
+    // [[D, 9, 5, 1],
+    //  [E, A, 6, 2],
+    //  [F, B, 7, 3],
+    //  [G, C, 8, 4]]
 }
 
 // TODO: Arbitrary n*n
