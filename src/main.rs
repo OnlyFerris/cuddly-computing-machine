f1acba4b758b2d96c0fa0342cb49e2245b9303cd
OnlyFerris <153568011+OnlyFerris@users.noreply.github.com>
2024-02-02 01:32:43 +0000

    Configure differently for tests, so that they can run. (No actual tests exist yet but that's beside the point.)

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,24 @@
 #![allow(internal_features)]
 #![no_std]
 #![no_main]
-// Necessary for `panic = "unwind"` builds on some platforms.
 #![feature(panic_unwind)]
-extern crate unwind;
 
-// Pull in the system libc library for what crt0.o likely requires.
+#[cfg(not(test))]
+extern crate unwind;
+#[cfg(not(test))]
 extern crate libc;
 
+#[cfg(test)]
+use core::ffi::c_char;
+#[cfg(test)]
+use core::ffi::c_int;
+#[cfg(not(test))]
 use core::ffi::{c_char, c_int};
+#[cfg(not(test))]
 use core::panic::PanicInfo;
 use cuddly_computing_machine::m_1628;
 
-// Entry point for this program.
-#[no_mangle] // ensure that this symbol is included in the output as `main`
+#[no_mangle]
 extern "C" fn main(_argc: c_int, _argv: *const *const c_char) -> c_int {
     //let m = &mut [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
     // TODO: Reinterpret nested array above as contiguous. If possible.
@@ -29,10 +34,11 @@ extern "C" fn main(_argc: c_int, _argv: *const *const c_char) -> c_int {
     }
 }
 
-// These functions are used by the compiler, but not for an empty program like this.
-// They are normally provided by `std`.
+#[cfg(not(test))]
 #[lang = "eh_personality"]
 fn rust_eh_personality() {}
+
+#[cfg(not(test))]
 #[panic_handler]
 fn panic_handler(_info: &PanicInfo) -> ! {
     core::intrinsics::abort()
